d11bfebcfb4fbd1c8caa99b35f0b0dfa00354a82
Luray He <c-lhe@ambarella.com>
2026-07-02 11:04:01 -0700

    userdefcall export support

--- source/ambapb-subgraph.js
+++ source/ambapb-subgraph.js
@@ -5,8 +5,12 @@
  * Most subgraph extract logic was already built in model-editor.js
  * Author: Luray He
  */
-import { applyBatchInlineExpansions } from './ambapb-batch-inline.js';
-import { SubgraphExtractError, promoteVisibleGraphOutputs } from './model-editor.js';
+import {
+    applyBatchInlineExpansions,
+    inlineExpansionBatchCallName,
+    resolveBatchCallTarget
+} from './ambapb-batch-inline.js';
+import { cloneGraph, SubgraphExtractError, promoteVisibleGraphOutputs } from './model-editor.js';
 
 const INLINE_NAME_PREFIX = /^inline::[^:]+::(.*)$/;
 
@@ -135,6 +139,117 @@ const remapArgument = (argument) => {
     return Object.assign({}, argument, {
         value: argumentValues(argument).map((entry) => remapValue(entry))
     });
+};
+
+export const cloneFragSubgraphNode = (fragNode) => {
+    const cloneGraphAttribute = (attribute) => {
+        if (attribute.type === 'graph' && attribute.value) {
+            return {
+                name: attribute.name,
+                type: attribute.type,
+                value: cloneGraph(attribute.value)
+            };
+        }
+        return {
+            name: attribute.name,
+            type: attribute.type,
+            value: attribute.value
+        };
+    };
+    return {
+        name: fragNode.name,
+        type: fragNode.type ? Object.assign({}, fragNode.type) : null,
+        attributes: (fragNode.attributes || []).map(cloneGraphAttribute),
+        blocks: (fragNode.blocks || []).map(cloneGraphAttribute),
+        inputs: (fragNode.inputs || []).map((input) => ({
+            name: input.name,
+            value: (input.value || []).map((val) => Object.assign({}, val))
+        })),
+        outputs: (fragNode.outputs || []).map((output) => ({
+            name: output.name,
+            value: (output.value || []).map((val) => Object.assign({}, val))
+        }))
+    };
+};
+
+export const ensureFragSubgraphGraphAttributes = (graph) => {
+    for (const node of graph.nodes || []) {
+        if (node.type?.name !== 'FragSubgraph' && node.type?.name !== 'UserDefSubgraph') {
+            continue;
+        }
+        for (const entry of [...(node.attributes || []), ...(node.blocks || [])]) {
+            if (entry.type === 'graph' && entry.value) {
+                entry.value = cloneGraph(entry.value);
+            }
+        }
+    }
+};
+
+export const collectReferencedSubgraphDefinitions = (extracted, workingGraph, extractSourceGraph, options = {}) => {
+    const callsToInline = options.callsToInline || new Set();
+    const definitions = [];
+    const seen = new Set();
+    const addDefinition = (definitionNode) => {
+        if (!definitionNode || !definitionNode.name || seen.has(definitionNode.name)) {
+            return;
+        }
+        if ((extracted.nodes || []).some((node) => node.name === definitionNode.name)) {
+            return;
+        }
+        seen.add(definitionNode.name);
+        definitions.push(definitionNode);
+    };
+
+    for (const node of extracted.nodes || []) {
+        if (node.type?.name === 'BatchCall' || node.type?.name === 'UserDefCall') {
+            const target = resolveBatchCallTarget(workingGraph, node);
+            if (target && target.fragSubgraphNode) {
+                addDefinition(target.fragSubgraphNode);
+            }
+        }
+    }
+
+    for (const node of extracted.nodes || []) {
+        const batchCallName = inlineExpansionBatchCallName(node);
+        if (!batchCallName) {
+            continue;
+        }
+        const originalBatchCall = (extractSourceGraph.nodes || []).find((entry) => entry.name === batchCallName);
+        if (!originalBatchCall) {
+            continue;
+        }
+        const target = resolveBatchCallTarget(extractSourceGraph, originalBatchCall);
+        if (target && target.fragSubgraphNode) {
+            addDefinition(target.fragSubgraphNode);
+        }
+    }
+
+    for (const batchCallName of callsToInline) {
+        const originalBatchCall = (extractSourceGraph.nodes || []).find((entry) => entry.name === batchCallName);
+        if (!originalBatchCall) {
+            continue;
+        }
+        const target = resolveBatchCallTarget(extractSourceGraph, originalBatchCall);
+        if (target && target.fragSubgraphNode) {
+            addDefinition(target.fragSubgraphNode);
+        }
+    }
+
+    return definitions;
+};
+
+export const appendReferencedSubgraphDefinitions = (extracted, workingGraph, extractSourceGraph, options = {}) => {
+    const definitions = collectReferencedSubgraphDefinitions(extracted, workingGraph, extractSourceGraph, options);
+    for (const definition of definitions) {
+        extracted.nodes.push(cloneFragSubgraphNode(definition));
+    }
+    return extracted;
+};
+
+export const isSubgraphDefinitionNode = (node) => {
+    return Boolean(node && node.type && (
+        node.type.name === 'FragSubgraph' || node.type.name === 'UserDefSubgraph'
+    ));
 };
 
 export const stripInlineExpansionPrefixes = (graph) => {
